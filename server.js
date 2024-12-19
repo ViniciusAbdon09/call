6b65867299f0561340cf1f3145a95b5ac8e4fe2d
Vinícius Abdon <77764863+ViniciusAbdon09@users.noreply.github.com>
2024-12-18 22:45:09 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -49,19 +49,12 @@ io.on('connection', (socket) => {
   console.log('Novo cliente conectado:', socket.id);
 
   socket.on('join-room', (roomId, userId) => {
+    console.log(`Usuário ${userId} entrou na sala ${roomId}`);
     socket.join(roomId);
     socket.to(roomId).emit('user-connected', userId);
-    
-    // Adicionar os novos eventos de gravação aqui
-    socket.on('start-recording', (userId) => {
-      socket.to(roomId).emit('recording-started', userId);
-    });
-
-    socket.on('stop-recording', () => {
-      socket.to(roomId).emit('recording-stopped');
-    });
 
     socket.on('disconnect', () => {
+      console.log(`Usuário ${userId} desconectou`);
       socket.to(roomId).emit('user-disconnected', userId);
     });
   });
